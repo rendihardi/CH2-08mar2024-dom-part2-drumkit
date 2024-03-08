82c5caba12898472931d7d64e908f21d16a75716
rendihardi <rendirenhard@gmail.com>
2024-03-08 22:20:29 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,65 @@
+function handleClick() {
+  alert("Handle Klik");
+}
+
+var drumButtons = document.querySelectorAll(".drum");
+console.log(drumButtons);
+
+for (let i = 0; i < drumButtons.length; i++) {
+  drumButtons[i].addEventListener("click", function () {
+    let btnInnerHTML = this.innerHTML;
+    buttonAnimation(btnInnerHTML);
+  });
+}
+
+//Deteksi keyboard yang ditekan
+document.addEventListener("keypress", function (event) {
+  makeSound(event.key);
+  buttonAnimation(event.key);
+});
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      var audio = new Audio("sounds/tom-1.mp3");
+      audio.play();
+      break;
+    case "a":
+      var audio = new Audio("sounds/tom-2.mp3");
+      audio.play();
+      break;
+    case "s":
+      var audio = new Audio("sounds/tom-3.mp3");
+      audio.play();
+      break;
+    case "d":
+      var audio = new Audio("sounds/tom-4.mp3");
+      audio.play();
+      break;
+    case "j":
+      var audio = new Audio("sounds/snare.mp3");
+      audio.play();
+      break;
+    case "k":
+      var audio = new Audio("sounds/crash.mp3");
+      audio.play();
+      break;
+    case "l":
+      var audio = new Audio("sounds/kick-bass.mp3");
+      audio.play();
+      break;
+    default:
+      alert("gada button nya");
+  }
+}
+
+function buttonAnimation(key) {
+  let activeBtn = document.querySelector("." + key);
+  activeBtn.classList.add("pressed");
+  activeBtn.classList.add("white");
+
+  setTimeout(function () {
+    activeBtn.classList.remove("pressed");
+    activeBtn.classList.remove("white");
+  }, 100);
+}
